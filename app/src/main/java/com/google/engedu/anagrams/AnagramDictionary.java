e8e7ac40b69d27ad2ab54fac6d79f2f489440720
Atews16 <swetasamaddar16@gmail.com>
2017-10-06 13:52:37 +0530

    Initial commit

---
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,177 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.anagrams;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private static final HashMap<String,ArrayList<String>>LettertoWord=new HashMap<>();
+    private static final HashSet<String>WordSet=new HashSet<>();
+    private static final ArrayList<String>dic=new ArrayList<>();
+    private Random random = new Random();
+
+
+    public AnagramDictionary(Reader reader) throws IOException {
+        BufferedReader in = new BufferedReader(reader);
+        String line;
+        while((line = in.readLine()) != null) {
+            String word = line.trim();
+            WordSet.add(word);
+            String sort=SortLetters(word);
+            if(LettertoWord.containsKey(sort))
+            {
+                ArrayList<String>value=LettertoWord.get(sort);
+                value.add(word);
+                LettertoWord.put(sort,value);
+            }
+            else
+            {
+                ArrayList<String>value=new ArrayList<>();
+                value.add(word);
+                LettertoWord.put(sort,value);
+            }
+            dic.add(word);
+        }
+    }
+
+    public boolean isGoodWord(String word, String base) {
+        if(word.contains(base))
+            return false;
+        if(WordSet.contains(word))
+            return true;
+        return false;
+
+    }
+
+    public List<String> getAnagrams(String targetWord) {
+     //   ArrayList<String> result = new ArrayList<String>();
+    /*   if(dictionary.containsValue(targetWord))
+           result.add(targetWord);
+*/
+    String sort1=SortLetters(targetWord);
+   /* for(String word:dic)
+    {
+        if(word.length()==targetWord.length())
+        {
+            String sort=SortLetters(word);
+           if(sort.equals(sort1))
+                result.add(word);
+        }
+    }*/
+
+       if(LettertoWord.containsKey(sort1))
+
+           return LettertoWord.get(sort1);
+        else
+            return null;
+       // return result;
+    }
+    public String SortLetters(String word)
+    {
+        char letter[]=word.toCharArray();
+        Arrays.sort(letter);
+        String neww=new String(letter);
+        return neww;
+    }
+
+    public List<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+        for(char a='a';a < 'z';a++){
+            ArrayList<String> anagram = new ArrayList<>();
+          String temp = word;
+            temp+=a;
+            temp = SortLetters(temp);
+            if (LettertoWord.containsKey(temp)) {
+                anagram = LettertoWord.get(temp);
+            }
+            for(int i = 0;i < anagram.size();i++){
+                if(isGoodWord(anagram.get(i),word)){
+                    result.add(anagram.get(i));
+                }
+
+            }
+
+        }
+
+
+        /*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        char intial='A';
+        String anotherword=word;
+        while(intial<='Z')
+        {
+            word=intial+anotherword;
+            word=SortLetters(word);
+            Log.d("getAna", word);
+            if(LettertoWord.containsKey(word))
+                result.addAll(LettertoWord.get(word));
+            //word="";
+            intial+=1;
+        }
+*/
+        return result;
+    }
+
+    public String pickGoodStarterWord() {
+
+        while(true) {
+
+                int ran = random.nextInt() % 1000;
+            if(ran>=0 && ran<1000) {
+                String word = dic.get(ran);
+                String key = SortLetters(word);
+                if (LettertoWord.get(key).size() >=MIN_NUM_ANAGRAMS)
+                    return dic.get(ran);
+            }
+        }
+    }
+}
